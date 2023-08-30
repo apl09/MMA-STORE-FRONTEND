77037e5e1d0dbb35544a845d3a36e08354b5cecc
apl09 <adrianpastorlopez09@gmail.com>
2023-08-30 17:55:18 +0200

    not working

--- src/context/ProductsContext/ProductsReducer.js
+++ src/context/ProductsContext/ProductsReducer.js
@@ -1,23 +1,25 @@
 const products = (state, action) => {
-  switch (action.type) {
-    case "GET_PRODUCTS":
-      return {
-        ...state,
-        products: action.payload,
-      };
-      case "ADD_CART":
-      return {
-        ...state,
-        cart: [action.payload,...state.cart]
-      };
-      case "CLEAR_CART":
-      return {
-        ...state,
-        cart: []
-      };
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+      case "GET_PRODUCTS":
+        return {
+          ...state,
+          products: action.payload,
+        };
+        case "ADD_CART":
+        return {
+          ...state,
+          cart: [action.payload,...state.cart]
+        };
+        case "CLEAR_CART":
+        return {
+          ...state,
+          cart: []
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default products;
 
-export default products;
+
